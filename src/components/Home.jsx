628f89b3dc1f54dfbd45986bf941383a4f9c86ec
Chahat Popli <85789202+chahatpopli@users.noreply.github.com>
2022-02-05 23:32:50 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,27 +3,13 @@ import { Box, makeStyles, Typography } from "@material-ui/core";
 import Homepage from "../Assets/Homepage.png"
 
 const Home = () => {
-    return ( <
-        Box >
-        <
-        Typography style = {
-            { textAlign: "center" }
-        }
-        variant = "h2" > Welcome! < /Typography> <
-        Typography style = {
-            { textAlign: "center" }
-        }
-        variant = "h5" > CRUD app with Json - server using Axios < /Typography> <
-        img src = { Homepage }
-        style = {
-            { width: "30%", margin: "5px 0 0 35%" }
-        }
-        /> <
-        Typography style = {
-            { textAlign: "center" }
-        }
-        variant = "h5" > Home Page < /Typography> < /
-        Box >
+    return (
+        <Box >
+            <Typography style = {{ textAlign: "center" }} variant = "h2" > Welcome! < /Typography> 
+            < Typography style = {{ textAlign: "center" }} variant = "h5" > CRUD app with Json - server using Axios < /Typography> 
+            <img src = { Homepage } style = {{ width: "30%", margin: "5px 0 0 35%" }} /> 
+            <Typography style = {{ textAlign: "center" }} variant = "h5" > Home Page < /Typography> 
+        </Box >
     );
 };
 
