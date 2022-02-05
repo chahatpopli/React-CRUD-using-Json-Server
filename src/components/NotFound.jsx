4fd5c5fbd9b41857e9ae7dcc8a0cd7d2bfa7ea00
Chahat Popli <85789202+chahatpopli@users.noreply.github.com>
2022-02-05 23:25:50 +0530

    Update NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import notfound from "../Assets/notfound.png"
 
 const NotFound = () => {
-    return ( < img src = { notfound }
-        style = {
-            { width: "40%", margin: "10px 0 0 30%" }
-        }
-        />
+    return ( 
+        < img src = { notfound } style = {{ width: "40%", margin: "10px 0 0 30%" }} />
     );
 };
 
