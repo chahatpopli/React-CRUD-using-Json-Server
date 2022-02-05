8740105ffa78e3031e5d4772d4a7879ad58c22ce
Chahat Popli <85789202+chahatpopli@users.noreply.github.com>
2022-02-05 23:09:18 +0530

    Create AddUser.jsx

---
+++ src/components/AddUser.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Box, makeStyles, FormGroup, FormControl, InputLabel, TextField, Input, Button, Typography } from "@material-ui/core";
+import { addUser } from "../Service/Api";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles({
+  container: {
+    width: "50%",
+    margin: "5% 0 0 25%",
+    "& > *": {
+      marginTop: "20px"
+    }
+  },
+  button: {
+    backgroundColor: "#0DCAF0"
+  }
+});
+
+const initialValues = {
+  name: "",
+  username: "",
+  email: "",
+  phone: ""
+};
+
+const AddUser = () => {
+  const [user, setUser] = useState(initialValues);
+  const { name, username, email, phone } = user;
+  const classname = useStyles();
+  const history = useNavigate();
+
+  const valueChange = (e) => {
+    console.log(e.target.value);
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const addUserDetail = async () => {
+    await addUser(user);
+    history("/all");
+  };
+
+  return (
+    <Box>
+      <FormGroup className={classname.container}>
+        <Typography variant="h3"> Add User </Typography>
+        <FormControl>
+          <InputLabel> Name </InputLabel>
+          <Input onChange={(e) => valueChange(e)} name="name" value={name} />
+        </FormControl>
+        <FormControl>
+          <InputLabel> User Name </InputLabel>
+          <Input
+            onChange={(e) => valueChange(e)}
+            name="username"
+            value={username}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel> Email </InputLabel>
+          <Input onChange={(e) => valueChange(e)} name="email" value={email} />
+        </FormControl>
+        <FormControl>
+          <InputLabel> Phone No. </InputLabel>
+          <Input onChange={(e) => valueChange(e)} name="phone" value={phone} />
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => addUserDetail()}
+        >
+          Add User
+        </Button>
+      </FormGroup>
+    </Box>
+  );
+};
+
+export default AddUser;
